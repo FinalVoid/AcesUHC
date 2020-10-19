768e5902086059eb4f0773140d086aaebc32d0fa
final <finalvoiddev@gmail.com>
2020-10-19 12:00:02 +0200

    Tracking dead players

--- src/main/java/net/aksyo/game/managers/TeamManager.java
+++ src/main/java/net/aksyo/game/managers/TeamManager.java
@@ -2,17 +2,19 @@ package net.aksyo.game.managers;
 
 import net.aksyo.game.roles.RoleType;
 import net.aksyo.game.roles.Team;
-import net.aksyo.game.roles.teams.*;
+import net.aksyo.game.teams.*;
 import net.aksyo.player.AcePlayer;
 import org.bukkit.entity.Player;
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
 
 public class TeamManager {
 
     private Map<Team, HashSet<AcePlayer>> TEAMS = new HashMap<>();
+    private LinkedList<AcePlayer> deadPlayers = new LinkedList<>();
 
     public TeamManager() {
         TEAMS.put(PiquesTeam.getInstance(), new HashSet<>());
@@ -24,6 +26,14 @@ public class TeamManager {
 
     public void setTeam(Player player, Team team, RoleType roleType) {
         TEAMS.get(team).add(new AcePlayer(player, team, roleType));
+    }
+
+    public boolean isPlayerDead(AcePlayer player) {
+        return deadPlayers.contains(player);
+    }
+
+    public boolean revivePlayer(AcePlayer player) {
+        return deadPlayers.remove(player);
     }
 
 
