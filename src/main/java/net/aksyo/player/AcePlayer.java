5b04702d9ce930152d6e305455879a2bf844a84e
final <finalvoiddev@gmail.com>
2020-10-19 12:00:13 +0200

    New player options

--- src/main/java/net/aksyo/player/AcePlayer.java
+++ src/main/java/net/aksyo/player/AcePlayer.java
@@ -1,8 +1,10 @@
 package net.aksyo.player;
 
+import net.aksyo.AcesUHC;
 import net.aksyo.game.roles.RoleType;
 import net.aksyo.game.roles.Team;
-import net.aksyo.game.roles.teams.JokerTeam;
+import net.aksyo.game.teams.JokerTeam;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 
 public class AcePlayer {
@@ -10,11 +12,13 @@ public class AcePlayer {
     private Player player;
     private RoleType roleType;
     private Team team;
+    private PlayerOption option;
 
     public AcePlayer(Player player, Team team, RoleType roleType) {
         this.player = player;
         this.roleType = roleType;
         this.team = roleType.get().isJoker() ? JokerTeam.getInstance() : team;
+        option = PlayerOption.PLAYER;
     }
 
     public Player getPlayer() {
@@ -27,5 +31,22 @@ public class AcePlayer {
 
     public Team getTeam() {
         return team;
+    }
+
+    public boolean isOption(PlayerOption option) {
+        return option == this.option;
+    }
+
+    public void kill() {
+        option = PlayerOption.SPECTATOR;
+        player.setGameMode(GameMode.SPECTATOR);
+    }
+
+    public boolean revive() {
+
+        if (AcesUHC.getInstance().getTeamManager().isPlayerDead(this) && option == PlayerOption.SPECTATOR) {
+            return AcesUHC.getInstance().getTeamManager().revivePlayer(this);
+        }
+        return false;
     }
 }
---
+++ src/main/java/net/aksyo/player/PlayerOption.java
@@ -0,0 +1,9 @@
+package net.aksyo.player;
+
+public enum PlayerOption {
+
+    SPECTATOR,
+    PLAYER,
+
+
+}
