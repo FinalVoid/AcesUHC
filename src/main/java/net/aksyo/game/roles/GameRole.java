c6d0152454075221651ec6408c1cd4d307f040bf
final <finalvoiddev@gmail.com>
2020-10-28 15:57:40 +0100

    Added powers

--- src/main/java/net/aksyo/game/roles/GameRole.java
+++ src/main/java/net/aksyo/game/roles/GameRole.java
@@ -1,5 +1,9 @@
 package net.aksyo.game.roles;
 
+import net.aksyo.player.AcePlayer;
+
+import java.util.function.Consumer;
+
 public abstract class GameRole {
 
 
@@ -8,6 +12,8 @@ public abstract class GameRole {
 
     public abstract String getDescription();
     public abstract String[] getInformation();
+
+    public abstract Consumer<AcePlayer> applyPowers();
 
     public abstract boolean isJoker();
 
